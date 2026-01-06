d1d459d6b50f9352e36ade61236f01078f960d4d
scott96707 <scott96707@yahoo.com>
2026-01-06 09:53:12 -0700

    feat: Increased wezterm opacity 75->85%

--- modules/common/wezterm.nix
+++ modules/common/wezterm.nix
@@ -3,7 +3,7 @@
 {
   programs.wezterm = {
     enable = true;
-    
+
     extraConfig = ''
       local wezterm = require 'wezterm'
       local config = wezterm.config_builder()
@@ -14,7 +14,7 @@
       config.font_size = 13.0
 
       -- 2. Window Settings
-      config.window_background_opacity = 0.75
+      config.window_background_opacity = 0.85
       config.window_decorations = "RESIZE"
       config.hide_tab_bar_if_only_one_tab = true
 
